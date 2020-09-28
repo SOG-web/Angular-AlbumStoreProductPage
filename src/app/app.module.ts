f492ee26121d0e57f6600586a10243daf1186309
SOG-web <raheemolalekanusman84@gmail.com>
2020-09-28 20:51:29 +0100

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from "@angular/router";
-
 import { AppComponent } from './app.component';
-import { ProductService } from "./product.service";
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+
+import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './product-list/product-list.component'
+
+import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  {
-    path: "products",
-    component: ProductListComponent,
-
-  },
-  {
-    path: "product/:id",
-    component: ProductPageComponent,
-  },
-  {
-    path: '',
-    redirectTo: products,
-    pathMatch: full
-  }
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
 ]
 
 @NgModule({
@@ -42,9 +28,7 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [
-    ProductService
-  ],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
