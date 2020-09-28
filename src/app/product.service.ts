c22133b3fbc4e73b087bdc79b69e5e517ff591c2
SOG-web <raheemolalekanusman84@gmail.com>
2020-09-28 12:26:13 +0100

    album and track interface added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,8 +11,8 @@ export class ProductService {
   constructor(private _http: Http) { }
 
 getAlbum(id: number): Observable<Album> {
-  this._http.get(this._albumUrl).map((response:Album) => {
-    return response.json(
+  this._http.get(this._albumUrl).map((response) => {
+    return <Album>response.json(
   })
 }
 
